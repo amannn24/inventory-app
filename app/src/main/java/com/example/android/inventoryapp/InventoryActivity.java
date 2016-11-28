4f20b7557739f7c28e3ce9bf8d83fcab5abee74a
Chris Dziewa <chris.dziewa@gmail.com>
2016-11-28 12:35:24 -0600

    Add EditorActivity and working floating action button

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.inventoryapp;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
@@ -13,6 +16,15 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        final FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.add_product_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent editorIntent = new Intent(InventoryActivity.this, EditorActivity.class);
+                startActivity(editorIntent);
+            }
+        });
 
         displayDatabaseInfo();
     }
@@ -28,12 +40,13 @@ public class InventoryActivity extends AppCompatActivity {
 
         Cursor cursor = getContentResolver().query(ProductEntry.CONTENT_URI, projection, null, null, null);
 
+        if (cursor != null) {
+            int count = cursor.getCount();
 
-        int count = cursor.getCount();
-
-        TextView textView = (TextView) findViewById(R.id.db_count);
-        textView.setText("Number of rows in the database: " + String.valueOf(count));
-
+            TextView textView = (TextView) findViewById(R.id.db_count);
+            textView.setText("Number of rows in the database: " + String.valueOf(count));
+            cursor.close();
+        }
     }
 
     // TODO: Add AsyncLoader
