dd6797bfbaa7ca24f6c6962023175a0ce913c5e1
Chris Dziewa <chris.dziewa@gmail.com>
2016-11-28 18:38:13 -0600

    Add async loader and basic list display

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,16 +1,23 @@
 package com.example.android.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
 
-public class InventoryActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PRODUCT_LOADER = 2;
+
+    private ProductCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,18 @@ public class InventoryActivity extends AppCompatActivity {
             }
         });
 
-        displayDatabaseInfo();
+        ListView productList = (ListView) findViewById(R.id.product_list);
+        mAdapter = new ProductCursorAdapter(this, null);
+        productList.setAdapter(mAdapter);
+
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+
     }
 
     private void displayDatabaseInfo() {
 
         String[] projection = new String[] {
+                ProductEntry._ID,
                 ProductEntry.COLUUMN_PRODUCT_NAME,
                 ProductEntry.COLUMN_PRODUCT_PRICE,
                 ProductEntry.COLUMN_PRODUCT_QUANTITY,
@@ -58,10 +71,38 @@ public class InventoryActivity extends AppCompatActivity {
                 result += name + " - " + price + " - " + quantity + "\n\n";
             }
 
-            TextView textView = (TextView) findViewById(R.id.db_count);
-            textView.setText(result);
             cursor.close();
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case PRODUCT_LOADER:
+                String[] projection = new String[] {
+                        ProductEntry._ID,
+                        ProductEntry.COLUUMN_PRODUCT_NAME,
+                        ProductEntry.COLUMN_PRODUCT_PRICE,
+                        ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                        ProductEntry.COLUMN_PRODUCT_IMAGE
+                };
+
+                // get Cursor loader
+                return new CursorLoader(getApplicationContext(), ProductEntry.CONTENT_URI, projection, null, null, null);
+            default:
+                // Loader not recognized
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+        mAdapter.swapCursor(null);
     }
 
     // TODO: Add AsyncLoader
---
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -0,0 +1,53 @@
+package com.example.android.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp.data.ProductContract;
+
+/**
+ * Created by Chris on 11/28/2016.
+ */
+
+public class ProductCursorAdapter extends CursorAdapter {
+
+
+    /**
+     * Constructs a new {@link #ProductCursorAdapter}
+     * @param context
+     * @param c the cursor that holds the data
+     */
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.product_list_item, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price_text_view);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity_text_view);
+
+        int nameIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUUMN_PRODUCT_NAME);
+        int priceIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantityIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+
+        String name = cursor.getString(nameIndex);
+        // Get the price back to decimal format
+        String price = Double.toString(cursor.getDouble(priceIndex) / 100);
+        String quantity = Integer.toString(cursor.getInt(quantityIndex));
+
+        nameTextView.setText(name);
+        priceTextView.setText(price);
+        quantityTextView.setText(quantity);
+    }
+}
